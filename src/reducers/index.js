9fe499fa475bb58efc10657c8b3eb4c9b414291f
jsbonso <jsbonso@gmail.com>
2018-06-01 16:30:32 +1000

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,7 @@
 "use strict"
 import {combineReducers} from 'redux';
-
-// HERE IMPORT REDUCERS TO BE COMBINED
 import {peopleReducers} from './peopleReducers';
 
-
-//HERE COMBINE THE REDUCERS
 export default combineReducers({
     people: peopleReducers,
 })
